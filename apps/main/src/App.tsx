10af27107efe6e231366269d09bc2633ae869eef
GMolter <gmolter8@gmail.com>
2025-07-19 23:37:31 -0400

    Updated App.tsx

--- apps/main/src/App.tsx
+++ apps/main/src/App.tsx
@@ -1,4 +1,4 @@
-import '../App.css';
+import './App.css'; // ✅ correct path now
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
