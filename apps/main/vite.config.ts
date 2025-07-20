51daaa27ba7f0c83b77e994378ae8bbf512020c1
GMolter <gmolter8@gmail.com>
2025-07-19 23:54:07 -0400

    Updated vite.config.ts

--- apps/main/vite.config.ts
+++ apps/main/vite.config.ts
@@ -1,12 +1,15 @@
-// vite.config.ts (in apps/main)
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname, 'src'),
   plugins: [react()],
+  root: './apps/main', // 👈 tell Vite where the actual app root is
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
-  }
-})
+    outDir: '../../dist', // ⬅ so the dist isn't inside apps/main
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, 'apps/main/index.html'),
+    },
+  },
+});
