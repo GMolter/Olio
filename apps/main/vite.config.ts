3bb79c811304f294ee57dab2f6577a0dfe484b9c
GMolter <gmolter8@gmail.com>
2025-07-19 23:04:23 -0400

    Updated vite.config.ts

--- apps/main/vite.config.ts
+++ apps/main/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: fileURLToPath(new URL('.', import.meta.url)),
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-})
+});
