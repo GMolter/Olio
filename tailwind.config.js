8f020c25498f6f1f7afe8114e19488f24e596ce8
GMolter <gmolter8@gmail.com>
2025-07-19 23:44:30 -0400

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './apps/**/*.{html,js,ts,jsx,tsx}',
+    './apps/main/src/**/*.{js,ts,jsx,tsx,html}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
