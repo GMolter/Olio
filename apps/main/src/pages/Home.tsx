cdbc1276bc37d13ebeadd403aa08470dee69ce21
GMolter <gmolter8@gmail.com>
2025-07-19 23:28:01 -0400

    Updated Home.tsx

---
+++ apps/main/src/pages/Home.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <div className="landing-container">
+      <div className="splotch splotch-1"></div>
+      <div className="splotch splotch-2"></div>
+      <div className="splotch splotch-3"></div>
+      <div className="splotch splotch-4"></div>
+      <div className="splotch splotch-5"></div>
+      <div className="splotch splotch-6"></div>
+
+      <div className="content">
+        <h1 className="title">Olio.one</h1>
+        <p className="subtitle">Site coming soon</p>
+        <div className="pulse-dot"></div>
+      </div>
+    </div>
+  );
+}
